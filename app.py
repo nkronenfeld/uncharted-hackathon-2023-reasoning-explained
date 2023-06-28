fadd2f523cc58da19d1861e6116f12f8ffd5b3a4
Nathan Kronenfeld <nkronenfeld@uncharted.software>
2023-06-28 13:57:08 -0400

    Add another endpoint for the article breakdown I'm working on

--- app.py
+++ app.py
@@ -67,6 +67,28 @@ def generate_argument_prompt(animal):
     return """Given the following argument, please identify the argument conclusion and the evidence supporting that conclusion.
     Argument:""".format(animal)
 
+
+@app.route("/article", methods=("GET", "POST"))
+def article_index():
+    if request.method == "POST":
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "what is the article we are discussing?"},
+                {"role": "assistant", "content": article},
+                {"role": "user", "content": "Please outline all arguments made in the article."}
+            ]
+        )
+        text_result = '\n'.join([c['message']['content'] for c in response.choices])
+        return redirect(url_for("article_index", result=text_result))
+
+    result = request.args.get("result")
+    if result is not None:
+        result = result.split('\n')
+    return render_template("article_index.html", result=result)
+
+
+
 def generate_prompt(animal):
     return """Suggest three names for an animal that is a superhero.
 
@@ -78,3 +100,30 @@ Animal: {}
 Names:""".format(
         animal.capitalize()
     )
+
+
+article = '''
+Nearly five years ago, Pittsburgh and the nation were traumatized when a gunman entered the Tree of Life synagogue building and killed 11 worshipers attending Shabbat morning services. This attack on the Jewish community — seemingly motivated by white supremacy and antisemitic hatred — left us all enraged and heartbroken. All Americans deserve to gather with their communities and practice their faiths in safety. The horrific violence at Tree of Life was an affront to that ideal.
+
+The victims, their families and the community deserve justice. The gunman, Robert G. Bowers, was ready to plead guilty and accept life in prison, but the Justice Department rejected that offer and moved forward with a capital murder trial. Bowers has now been found guilty, and the jury is hearing arguments over whether to impose the death penalty. But the government’s decision to pursue the death penalty, itself a product and perpetuation of white supremacy, cannot be the answer that brings healing and closure — or justice.
+
+The death penalty is a morally bankrupt and inescapably racist institution. The horrific acts the defendant was found guilty of do not change this reality. Nor does the fact that Bowers is a White man charged with acting out of racial and antisemitic hatred eradicate the racism and inequality entrenched in the death penalty. Indeed, the prosecution’s pursuit of death in this case has resulted in racial exclusion that harms the entire Pittsburgh community.
+
+A jury is supposed to represent the conscience of the community, but to win a verdict of death, lawyers from the Justice Department — including the Civil Rights Division, which is responsible for enforcing federal statutes prohibiting discrimination — participated in a selection process that excluded all Black and Latino people from the jury. Striking these members of the community was part and parcel of the DOJ’s strategy to pick the jurors it deemed most avid to choose capital punishment.
+
+The court sent 1,500 questionnaires to prospective jurors, and more than 200 appeared for questioning. These jurors were asked about hardship, their knowledge of the case and their potential bias and were subjected to a process known as death qualification. Sixty-eight made it past the interviews; only four were Black, and one was Latino. The government then struck all five of these jurors. The final jury, including alternates, consists of 17 White people and one Asian person.
+
+Death qualification is the first legal mechanism facilitating this exclusion of Black and Latino jurors. It requires that to be eligible to serve, jurors must declare they are not opposed to capital punishment. Death-qualified jurors must also be willing to vote to impose death if they find it to be the appropriate sentence. Decades of evidence shows that death qualification results in the disproportionate exclusion of groups that are more likely to oppose capital punishment, including Black people and other people of color as well as followers of certain religions. Death qualification also results in juries that are more likely to convict and more likely to reach hasty decisions.
+
+The second legal mechanism at play is the peremptory strike, which allows prosecutors to exclude otherwise eligible jurors — unless the defense can prove that the strikes were motivated by intentional racism or gender discrimination, an extremely difficult standard to meet. Prosecutors in this case used their discretionary strikes to remove the remaining Black and Latino jurors.
+
+Historical and current experiences of racial discrimination contribute to the distrust that many Black people and other people of color feel toward the death penalty, which in turn leads to systemic juror disqualification in capital cases. Our modern death penalty is an outgrowth of lynching and racial terror, with state-approved capital punishment gradually replacing racialized mob justice. And just like lynching, the death penalty today disproportionately kills Black people. Though Black Americans make up only about 13 percent of the population, they represent 34 percent of all executions between 1976 and 2022 and 41 percent of the current death row population. These facts contribute to the Black community’s distrust of and disdain for this extreme punishment. And even though this is a case without a Black defendant or a Black victim, Black voices in the Pittsburgh area remain part of the conscience of the community.
+
+With its selection of a nearly all-White jury, the Justice Department is stating that preserving its ability to win a death sentence justifies excluding Black and Latino jurors from one of the most vital instruments of our democracy. We should say no to this.
+
+The death penalty diminishes us all every time it transforms a legal proceeding into a parade of horrors played out in front of jurors whose most important qualification is their willingness to vote to kill. President Biden appeared to recognize this while campaigning on a still unfulfilled promise to end the federal death penalty. It’s not too late to make good on this commitment and halt the cycle of discrimination at the core of capital punishment. The first step: Disband this nonrepresentative jury and stop pursuing the death penalty in Pittsburgh.'''
+def generate_reasoning_prompt():
+    return f'''Please outline the arguments made in the following article:
+
+    {article}
+    '''
